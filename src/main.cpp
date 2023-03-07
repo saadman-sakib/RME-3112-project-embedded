55b19e6bd7894bd2d163e3328f753d67fe94fe14
Saadman Sakib <saadman.sakib2020@gmail.com>
2023-03-07 14:42:56 +0600

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,39 @@
 #include <Arduino.h>
+#include "Servo.h"
 
-const int BUFFER_SIZE = 4;
+const int BUFFER_SIZE = 5;
 char buf[BUFFER_SIZE];
+Servo servo;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(13, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.setTimeout(1);
+  servo.attach(3);
 }
 
 bool on = false;
+String data="";
 
 void loop() {
 
     // check if data is available
-    if (Serial.available() > 0) {
+    if (Serial.available()) {
       // read the incoming bytes:
-      int rlen = Serial.readBytes(buf, BUFFER_SIZE);
+      data = Serial.readStringUntil('\r');
 
       // prints the received data
       // Serial.print("I received: ");
       // for(int i = 0; i < rlen; i++)
       //   Serial.print(buf[i]);
-      if(buf[1]=='N'){
-        digitalWrite(13, HIGH);
-      } else {
-        digitalWrite(13,LOW);
+      if(data=="ON"){
+        // digitalWrite(13, HIGH);
+        servo.write(180);
+      } else if (data=="OFF") {
+        // digitalWrite(13,LOW);
+        servo.write(0);
       }
     }
 }
 
-
-// void loop() {
-
-// }
